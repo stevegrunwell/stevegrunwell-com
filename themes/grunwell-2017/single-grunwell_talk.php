eb90f1c7c7723c5694806faeff725d151cdd6989
Steve Grunwell <sgrunwell@liquidweb.com>
2017-11-06 02:50:21 +0000

    Since Lovecraft applies the background image based on the .post class, ensure it's present even on custom post types

--- themes/grunwell-2017/single-grunwell_talk.php
+++ themes/grunwell-2017/single-grunwell_talk.php
@@ -8,7 +8,7 @@
 
 			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-				<div id="post-<?php the_ID(); ?>" <?php post_class('single'); ?>>
+				<div id="post-<?php the_ID(); ?>" <?php post_class('single post'); ?>>
 
 					<div class="post-inner">
 
