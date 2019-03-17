1bb74f17a257d034699d42994ddf5135e6d14518
Steve Grunwell <steve@stevegrunwell.com>
2019-03-17 21:40:25 +0000

    Prevent duplicate IDs on the search form

--- themes/grunwell-2018/searchform.php
+++ themes/grunwell-2018/searchform.php
@@ -1,4 +1,11 @@
-<form method="get" class="search-form" id="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<input type="search" class="search-field" placeholder="<?php echo esc_attr( _x('Search&hellip;', 'search form placeholder', 'grunwell-2018') ); ?>" name="s" id="s" />
-	<button type="submit" class="search-button"><div class="genericon genericon-search"></button>
+<?php
+
+// Generate a unique suffix for IDs.
+$unique = uniqid();
+?>
+
+<form method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+	<label for="search-<?php echo esc_attr( $unique ); ?>" class="screen-reader-text">
+	<input type="search" class="search-field" placeholder="<?php echo esc_attr( _x('Search&hellip;', 'search form placeholder', 'grunwell-2018') ); ?>" name="s" id="search-<?php echo esc_attr( $unique ); ?>" />
+	<button type="submit" class="search-button" aria-label="<?php esc_attr_e( 'Submit search form', 'grunwell-2018' ); ?>"><div class="genericon genericon-search"></button>
 </form>
