20c84049d679726e1648171da0a0d25d6b1dbc07
Steve Grunwell <steve@stevegrunwell.com>
2019-03-17 22:32:11 +0000

    Properly close the search form label and ensure buttons display as intended by the parent theme

--- themes/grunwell-2018/searchform.php
+++ themes/grunwell-2018/searchform.php
@@ -5,7 +5,7 @@ $unique = uniqid();
 ?>
 
 <form method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<label for="search-<?php echo esc_attr( $unique ); ?>" class="screen-reader-text">
+	<label for="search-<?php echo esc_attr( $unique ); ?>" class="screen-reader-text"><?php esc_html_e( 'Search this site:', 'grunwell-2018' ); ?></label>
 	<input type="search" class="search-field" placeholder="<?php echo esc_attr( _x('Search&hellip;', 'search form placeholder', 'grunwell-2018') ); ?>" name="s" id="search-<?php echo esc_attr( $unique ); ?>" />
-	<button type="submit" class="search-button" aria-label="<?php esc_attr_e( 'Submit search form', 'grunwell-2018' ); ?>"><div class="genericon genericon-search"></button>
+	<button type="submit" class="search-button"><div class="genericon genericon-search"></div><span class="screen-reader-text"><?php _e( 'Search', 'grunwell-2018' ); ?></span></button>
 </form>
