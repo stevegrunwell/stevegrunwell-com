3d727a82d1eb001202a192bb9d250bf8c3ba07ac
Steve Grunwell <steve@stevegrunwell.com>
2020-06-16 16:44:22 +0000

    Merge changes in the searchform.php template

--- themes/grunwell-2018/searchform.php
+++ themes/grunwell-2018/searchform.php
@@ -1,11 +1,10 @@
 <?php
-
-// Generate a unique suffix for IDs.
-$unique = uniqid();
+$form_id = uniqid( 'search-form-' );
+$fieldid = uniqid( 's-' );
 ?>
 
-<form method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<label for="search-<?php echo esc_attr( $unique ); ?>" class="screen-reader-text"><?php esc_html_e( 'Search this site:', 'grunwell-2018' ); ?></label>
-	<input type="search" class="search-field" placeholder="<?php echo esc_attr( _x('Search&hellip;', 'search form placeholder', 'grunwell-2018') ); ?>" name="s" id="search-<?php echo esc_attr( $unique ); ?>" />
-	<button type="submit" class="search-button"><div class="genericon genericon-search"></div><span class="screen-reader-text"><?php _e( 'Search', 'grunwell-2018' ); ?></span></button>
+<form method="get" class="search-form" id="<?php echo esc_attr( $form_id ); ?>" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+	<label for="<?php echo esc_attr( $fieldid ); ?>" class="screen-reader-text"><?php esc_html_e( 'Search this site:', 'grunwell-2018' ); ?></label>
+	<input type="search" class="search-field" placeholder="<?php _x( 'Search&hellip;', 'search form placeholder', 'grunwell-2018' ); ?>" name="s" id="<?php echo esc_attr( $fieldid ); ?>" />
+	<button type="submit" class="search-button"><div class="genericon genericon-search"></div><span class="screen-reader-text"><?php _e( 'Search', 'lovecraft' ); ?></span></button>
 </form>
