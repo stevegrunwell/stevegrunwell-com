4d9f924d9abcca5e603643e06070eb63d1313285
Steve Grunwell <steve@stevegrunwell.com>
2021-01-03 20:21:43 -0500

    Don't enqueue the WP Block Library, as the site isn't using Gutenberg

--- themes/grunwell-2018/functions.php
+++ themes/grunwell-2018/functions.php
@@ -39,6 +39,9 @@ function enqueue_parent_theme_styles() {
 		null,
 		'print'
 	);
+
+	// Gutenberg block styles, which aren't needed.
+	wp_dequeue_style( 'wp-block-library' );
 }
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_parent_theme_styles', 100 );
 
